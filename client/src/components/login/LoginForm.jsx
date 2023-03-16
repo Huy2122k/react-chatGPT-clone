8ab626ff1d17c1e441d27a80fe299e5cfc913345
huy2122k <wwwhuy212@gmail.com>
2023-03-17 00:30:09 +0700

    add

--- client/src/components/login/LoginForm.jsx
+++ client/src/components/login/LoginForm.jsx
@@ -2,7 +2,6 @@ import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 import SvgComponent from "../SvgComponent";
-import "../signup/signupform.css";
 
 const SignupForm = () => {
   const [loginEmail, setLoginEmail] = useState("");
